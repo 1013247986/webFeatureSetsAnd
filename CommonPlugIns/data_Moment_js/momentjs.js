10c6781a2ec72103a4eef72b09254667061cecbc
LIWANG1998 <535287382@qq.com>
2020-12-01 10:18:42 +0800

    2020-12-01 : 添加js日期处理插件

---
+++ CommonPlugIns/data_Moment_js/momentjs.js
@@ -0,0 +1,1 @@
+// 具体见 http://momentjs.cn/
--- CommonPlugIns/journal.js
+++ CommonPlugIns/journal.js
@@ -3,3 +3,9 @@
  * @name 刘杰
  * @standard 增加：解决IE不兼容es6语法的插件 <Ie_Compatible_Es6> 文件夹
  */
+
+/**
+ * @time 2020-12-01 10：10
+ * @name 李旺
+ * @standard 增加：JavaScript 日期处理类库 <data_Moment_js> 文件夹
+ */
